b861f85f9ac2734ffc40a3834904f54c1261da9d
kozaktarasofficial@gmail.com <159Taras03>
2021-06-28 18:29:25 +0300

    v1.09

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+jQuery(document).ready(function($) {
+  "use strict";
+  $('#customers-testimonials').owlCarousel( {
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 30,
+    autoplay: true,
+    nav:true,
+    autoplayTimeout: 8500,
+    smartSpeed: 1000,
+    navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+});
