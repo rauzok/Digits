c47b0afbb071b4f9f60d02f3d6d580c6b5d47bbf
Taras Kozak <kozaktarasofficial@gmail.com>
2021-07-01 21:43:50 +0300

    Testimonials

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// eslint-disable-next-line no-undef
+jQuery(document).ready(function($) {
+  $('#customers-testimonials').owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 30,
+    autoplay: true,
+    nav: true,
+    autoplayTimeout: 8500,
+    smartSpeed: 1000,
+    navText: ['<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+});
